0c9c27af74b37ffa3187719bbb604513ced1c56a
Konstantin Goryachev <kevinglare@gmail.com>
2022-04-26 15:54:16 +0300

    refactor: test

--- hw6/hw_test.go
+++ hw6/hw_test.go
@@ -4,31 +4,31 @@ import "testing"
 
 func TestGeom_CalculateDistance(t *testing.T) {
 	tests := []struct {
-		name         string
-		geom         []float64
-		wantDistance float64
+		name string
+		args []float64
+		want float64
 	}{
 		{
-			name:         "#1",
-			geom:         []float64{1, 1, 4, 5},
-			wantDistance: 5,
+			name: "#1",
+			args: []float64{1, 1, 4, 5},
+			want: 5,
 		},
 		{
-			name:         "#2",
-			geom:         []float64{1, 1, 4, -5},
-			wantDistance: -1,
+			name: "#2",
+			args: []float64{1, 1, 4, -5},
+			want: -1,
 		},
 		{
-			name:         "#3",
-			geom:         []float64{0, 0, 0, 0},
-			wantDistance: 0,
+			name: "#3",
+			args: []float64{0, 0, 0, 0},
+			want: 0,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			distance := CalculateDistance(tt.geom[0], tt.geom[1], tt.geom[2], tt.geom[3]) // очень не нравится данный подход, жалко не работает tt.geom...
-			if distance != tt.wantDistance {
-				t.Errorf("CalculateDistance() = %v, want %v", distance, tt.wantDistance)
+			got := CalculateDistance(tt.args[0], tt.args[1], tt.args[2], tt.args[3]) // очень не нравится данный подход, жалко не работает tt.geom...
+			if got != tt.want {
+				t.Errorf("CalculateDistance() = %v, want %v", got, tt.want)
 			}
 		})
 	}
