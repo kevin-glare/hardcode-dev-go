7d5cf8307675ea579a19576e7b24f43c6003bdd6
Konstantin Goryachev <kevinglare@gmail.com>
2022-05-23 23:35:34 +0300

    fix: tests

--- hw9/pkg/task2/hw.go
+++ hw9/pkg/task2/hw.go
@@ -4,14 +4,16 @@ import (
 	"github.com/kevin-glare/hardcode-dev-go/hw9/pkg/task1"
 )
 
-func MaxAgeHuman(people ...task1.Interface) *task1.Interface {
-	var human *task1.Interface
+func MaxAgeHuman(people ...task1.Interface) task1.Interface {
+	var human task1.Interface
 	var maxAge int
 
-	for _, p := range people {
-		if p.Age() > maxAge {
-			maxAge = p.Age()
-			human = &p
+	for _, el := range people {
+		if p, ok := el.(task1.Interface); ok {
+			if p.Age() > maxAge {
+				maxAge = p.Age()
+				human = p
+			}
 		}
 	}
 
--- hw9/pkg/task2/hw_test.go
+++ hw9/pkg/task2/hw_test.go
@@ -7,41 +7,34 @@ import (
 )
 
 func TestMaxAgeHuman(t *testing.T) {
-	// Не нравится такой подход, пробовал testify.Assert и reflect.DeepEqual, но из-за указателей тесты не проходят (MaxAgeHuman() = &{2}, want &{2}).
-
-	employee1 := task1.NewEmployee(1)
-	employee2 := task1.NewEmployee(2)
-	customer1 := task1.NewCustomer(1)
-	customer2 := task1.NewCustomer(2)
-
 	tests := []struct {
 		name string
-		args []task1.Interface
-		want task1.Interface
+		args []interface{}
+		want interface{}
 	}{
 		{
-			name: "Test #1 - all types",
-			args: []task1.Interface{
-				customer1,
-				employee2,
+			name: "Test #1 - customer and employee",
+			args: []interface{}{
+				task1.NewCustomer(1),
+				task1.NewEmployee(2),
 			},
-			want: employee2,
+			want: task1.NewEmployee(2),
 		},
 		{
-			name: "Test #2 - only employees",
-			args: []task1.Interface{
-				customer1,
-				customer2,
+			name: "Test #2 - only customers",
+			args: []interface{}{
+				task1.NewCustomer(1),
+				task1.NewCustomer(2),
 			},
-			want: customer2,
+			want: task1.NewCustomer(2),
 		},
 		{
 			name: "Test #3 - only employees",
-			args: []task1.Interface{
-				employee1,
-				employee2,
+			args: []interface{}{
+				task1.NewEmployee(1),
+				task1.NewEmployee(2),
 			},
-			want: employee2,
+			want: task1.NewEmployee(2),
 		},
 	}
 
@@ -50,7 +43,7 @@ func TestMaxAgeHuman(t *testing.T) {
 			got := MaxAgeHuman(tt.args...)
 
 			if *got != tt.want {
-				t.Errorf("MaxAgeHuman() = %v, want %v", got, tt.want)
+				t.Errorf("MaxAgeHuman() = %+v, want %+v", *got, tt.want)
 			}
 		})
 	}
