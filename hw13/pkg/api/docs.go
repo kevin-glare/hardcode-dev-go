7641146e81a7aa573544f6d5ea981c0c573d7912
Konstantin Goryachev <kevinglare@gmail.com>
2022-06-07 11:26:42 +0300

    feat: hw13

---
+++ hw13/pkg/api/docs.go
@@ -0,0 +1,128 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/kevin-glare/hardcode-dev-go/hw5/pkg/crawler"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+func (api *Api) index(w http.ResponseWriter, r *http.Request) {
+	resp := &Response{Code: http.StatusOK}
+	query := r.URL.Query().Get("q")
+
+	if len(query) > 0 {
+		resp.Data = api.store.FindByQuery(query)
+	} else {
+		resp.Data = api.store.Docs
+	}
+
+	renderJSON(w, resp)
+}
+
+func (api *Api) create(w http.ResponseWriter, r *http.Request) {
+	resp := &Response{Code: http.StatusOK}
+	var mutex sync.Mutex
+	var doc crawler.Document
+
+	err := json.NewDecoder(r.Body).Decode(&doc)
+	if err != nil {
+		resp.Code = http.StatusBadRequest
+		renderJSON(w, resp)
+		return
+	}
+
+	mutex.Lock()
+	doc.ID = api.store.Docs[len(api.store.Docs)-1].ID + 1
+	api.store.Docs = append(api.store.Docs, doc)
+	mutex.Unlock()
+
+	resp.Data = doc
+	renderJSON(w, resp)
+}
+
+func (api *Api) read(w http.ResponseWriter, r *http.Request) {
+	resp := &Response{Code: http.StatusOK}
+
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		resp.Error = err.Error()
+		resp.Code = http.StatusBadRequest
+		renderJSON(w, resp)
+		return
+	}
+
+	doc := api.store.FindByID(id)
+	if doc == nil {
+		resp.Error = fmt.Sprintf("id %d not found\n", id)
+		resp.Code = http.StatusNotFound
+		renderJSON(w, resp)
+		return
+	}
+
+	resp.Data = &doc
+	renderJSON(w, resp)
+}
+
+func (api *Api) update(w http.ResponseWriter, r *http.Request) {
+	resp := &Response{Code: http.StatusOK}
+	var mutex sync.Mutex
+	var doc crawler.Document
+
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		resp.Error = err.Error()
+		resp.Code = http.StatusBadRequest
+		renderJSON(w, resp)
+		return
+	}
+
+	err = json.NewDecoder(r.Body).Decode(&doc)
+	if err != nil {
+		resp.Code = http.StatusBadRequest
+		renderJSON(w, resp)
+		return
+	}
+
+	mutex.Lock()
+	doc.ID = id
+	for i, d := range api.store.Docs {
+		if d.ID == doc.ID {
+			api.store.Docs[i] = doc
+			break
+		}
+	}
+	mutex.Unlock()
+
+	resp.Data = doc
+}
+
+func (api *Api) destroy(w http.ResponseWriter, r *http.Request) {
+	resp := &Response{Code: http.StatusOK}
+	var mutex sync.Mutex
+
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		resp.Error = err.Error()
+		resp.Code = http.StatusBadRequest
+		renderJSON(w, resp)
+		return
+	}
+
+	mutex.Lock()
+	for i, doc := range api.store.Docs {
+		if doc.ID == id {
+			api.store.Docs = append(api.store.Docs[:i], api.store.Docs[i+1:]...)
+			break
+		}
+	}
+	mutex.Unlock()
+
+	renderJSON(w, resp)
+}
---
+++ hw13/pkg/api/store.go
@@ -0,0 +1,91 @@
+package api
+
+import (
+	"fmt"
+	"github.com/kevin-glare/hardcode-dev-go/hw5/pkg/crawler"
+	"github.com/kevin-glare/hardcode-dev-go/hw5/pkg/crawler/spider"
+	"github.com/kevin-glare/hardcode-dev-go/hw5/pkg/index/hash"
+	"sort"
+)
+
+var (
+	urls  = [2]string{"https://go.dev", "https://golang.org"}
+	depth = 1
+)
+
+type Store struct {
+	index *hash.Index
+	Docs  []crawler.Document
+}
+
+func NewStore() *Store {
+	index := hash.New()
+
+	docs := parsingURLs(index)
+	sort.SliceStable(docs, func(i, j int) bool {
+		return docs[i].ID < docs[j].ID
+	})
+
+	return &Store{
+		index: index,
+		Docs:  docs,
+	}
+}
+
+func parsingURLs(index *hash.Index) []crawler.Document {
+	spider := spider.New()
+	counter := 0
+
+	var data []crawler.Document
+
+	for _, url := range urls {
+		result, err := spider.Scan(url, depth)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		for i, _ := range result {
+			result[i].ID = counter
+			counter++
+		}
+
+		data = append(data, result...)
+		index.Add(result)
+	}
+
+	return data
+}
+
+func (r *Store) FindByQuery(query string) []crawler.Document {
+	findDoc := make([]crawler.Document, 0)
+	indices := r.index.Search(query)
+
+	for _, val := range indices {
+		for _, doc := range r.Docs {
+			if doc.ID == val {
+				findDoc = append(findDoc, doc)
+				break
+			}
+		}
+	}
+
+	return findDoc
+}
+
+func (r *Store) FindByID(id int) *crawler.Document {
+	for i, d := range r.Docs {
+		if d.ID == id {
+			return &r.Docs[i]
+		}
+	}
+	return nil
+}
+
+func (r *Store) Update(id int) *crawler.Document {
+	return nil
+}
+
+func (r *Store) Destroy(id int) error {
+	return nil
+}
